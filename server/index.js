866beb195e934c6ee47d4659ef46f7386b8a65a1
udoh <66221563+udohahanor@users.noreply.github.com>
2023-09-03 11:38:28 +0100

    error function solved

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -12,6 +13,7 @@ const URI = process.env.URI;
 
 //ExpressMiddleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
 
@@ -20,7 +22,7 @@ app.get('/', (req, res) => {
     res.send('Homepage');
 });
 
-// get request
+// GET all requests
 app.get('/user', async (req, res) => {
     try {
         const user = await User.find({}).sort({ createdAt: -1 });
@@ -32,15 +34,70 @@ app.get('/user', async (req, res) => {
     }
 });
 
-// post request
+// GET a single user by ID
+app.get('/user/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({ message: `Cannot find user with id ${id}` });
+        }
+
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// POST a user
 app.post('/user', async (req, res) => {
     const { name, email } = req.body;
 
     try {
         const user = await User.create({ name, email });
-        res.status(200).json(user);
+        res.status(201).json(user);
     } catch (error) {
-        console.log(error.message);
+        console.error(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// UPDATE a user by ID
+app.put('/user/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findByIdAndUpdate(id, req.body);
+
+        if (!user) {
+            return res.status(404).json({ message: `Cannot find user with id ${id}` });
+        }
+
+        const updatedUser = await User.findById(id);
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//DELETE a request
+app.delete('/user/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findByIdAndDelete(id);
+        if (!user) {
+            return res.status(404).json({ message: `Cannot find user with id ${id}` });
+        }
+        res.status(200).json({ message: `User with id ${id} does not exist` });
+
+    } catch (error) {
+        console.error(error.message);
         res.status(500).json({ message: error.message });
     }
 });
@@ -56,4 +113,4 @@ mongoose.connect(URI)
         });
     }).catch((error) => {
         console.log(error);
-    })
+    });
